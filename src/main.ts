5d3f0bb70a4ac77854401e7ab64959d93caa6b9d
Bakhadyrov Firuzbek <bakhadyrovf@gmail.com>
2023-10-23 19:36:00 +0500

    ref: cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
         origin: ['http://localhost:3001', 'http://localhost:3000'],
+        methods: '*',
         credentials: true,
     });
     app.use(cookieParser(process.env.APP_SECRET));
