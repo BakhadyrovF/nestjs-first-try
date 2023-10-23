c010936f53b89510f06fafb8cce83826472d0adf
Bakhadyrov Firuzbek <bakhadyrovf@gmail.com>
2023-10-23 20:17:26 +0500

    rule for checking that inputs are not empty

--- src/authentication/dto/sign-in.dto.ts
+++ src/authentication/dto/sign-in.dto.ts
@@ -1,11 +1,13 @@
-import { IsEmail, MaxLength, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from "class-validator";
 
 
 export class SignInDTO {
+    @IsNotEmpty()
     @MaxLength(255)
     @IsEmail()
     email: string;
 
+    @IsNotEmpty()
     @MinLength(8)
     @MaxLength(128)
     password: string
--- src/authentication/dto/sign-up.dto.ts
+++ src/authentication/dto/sign-up.dto.ts
@@ -2,15 +2,18 @@ import { IsEmail, IsNotEmpty, Matches, MaxLength, MinLength } from "class-valida
 
 
 export class SignUpDTO {
+    @IsNotEmpty()
     @MaxLength(255)
     @IsEmail()
     email: string;
 
+    @IsNotEmpty()
     @MaxLength(50)
     @IsNotEmpty()
     @Matches('^[a-zA-Z0-9_.]+$')
     username: string;
 
+    @IsNotEmpty()
     @MinLength(8)
     @MaxLength(128)
     password: string
